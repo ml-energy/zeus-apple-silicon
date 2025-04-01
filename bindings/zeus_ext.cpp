cb29243f2d7a0e0eab860a3da1b6939556a06847
Jisang Ahn <michahn@umich.edu>
2025-04-01 19:41:54 -0400

    Implement first pieces of nanobind extension

---
+++ bindings/zeus_ext.cpp
@@ -0,0 +1,17 @@
+#include <nanobind/nanobind.h>
+#include "energy.hpp"
+
+namespace nb = nanobind;
+using namespace nb::literals;
+
+NB_MODULE(zeus_ext, m) {
+    m.doc() = "An API for programmatically measuring energy consumption on Apple silicon chips.";
+
+    nb::class_<Metrics>(m, "Metrics")
+        .def(nb::init<>())
+        .def_rw("cpu_mj", &Metrics::cpu_mj);
+
+    nb::class_<AppleEnergyMonitor>(m, "AppleEnergyMonitor")
+        .def(nb::init<>())
+        .def("get_cumulative_energy", &AppleEnergyMonitor::get_cumulative_energy);
+}
