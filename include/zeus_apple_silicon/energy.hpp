b50268a5bf33c2a91e9e6c4eda571c8d35dea2c8
Jisang Ahn <michahn@umich.edu>
2025-04-11 15:03:53 -0400

    Account for possibility of multiple efficiency/performance CPU managers

--- include/zeus_apple_silicon/energy.hpp
+++ include/zeus_apple_silicon/energy.hpp
@@ -207,9 +207,17 @@ private:
         }
         result.performance_cores_mj->push_back(energy);
       } else if (is_cpu_manager(channel_name, 'E')) {
-        result.efficiency_core_manager_mj = energy;
+        if (!result.efficiency_core_manager_mj) {
+          result.efficiency_core_manager_mj = energy;
+        } else {
+          result.efficiency_core_manager_mj += energy;
+        }
       } else if (is_cpu_manager(channel_name, 'P')) {
-        result.performance_core_manager_mj = energy;
+        if (!result.performance_core_manager_mj) {
+          result.performance_core_manager_mj = energy;
+        } else {
+          result.performance_core_manager_mj += energy;
+        }
       } else if (channel_name.find("DRAM") != std::string::npos) {
         result.dram_mj = energy;
       } else if (channel_name.find("GPU Energy") != std::string::npos) {
